750eed20d36833e9126156bbed36c55a25f0458d
Peter Babo <33118245+babopeter@users.noreply.github.com>
2023-11-08 11:51:09 +0100

    Indicate that dropDownItem is not used

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import Dropdown from './dropdown.js';
 
-const dropdownItem = new Dropdown(document.getElementById('element'));
+const _dropdownItem = new Dropdown(document.getElementById('element'));
